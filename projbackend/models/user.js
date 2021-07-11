ead883faf9c4c57b0d43455c7ecbfc1bec3df84e
shasank27 <shasankperiwal2027@gmail.com>
2021-07-11 07:27:26 +0530

    New Commit

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,6 @@
+import { v1 as uuidv1 } from "uuid";
 const mongoose = require("mongoose");
+const mongoose = require("crypto");
 
 var userSchema = mongoose.Schema({
   name: {
@@ -19,9 +21,9 @@ var userSchema = mongoose.Schema({
     unique: true,
   },
   //TODO:
-  password: {
+  encrypt_password: {
     type: String,
-    trim: true,
+    required: true,
   },
   salt: {
     type: String,
@@ -36,4 +38,32 @@ var userSchema = mongoose.Schema({
   },
 });
 
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encrypt_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this.password;
+  });
+
+userSchema.method({
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encrypt_password;
+  },
+
+  securePassword: function (plainPassword) {
+    if (plainPassword == "") return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+});
 module.exports = moongose.model("user", userSchema);
